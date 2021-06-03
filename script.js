5e553192bc96b9bf04ab2c67a7aafecdac5181b1
vanessa <vanessa.wagner@students.coding-bootcamps.eu>
2021-06-03 20:41:24 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const searchBtn = document.querySelector("#search-btn");
+const titleInput = document.querySelector("#input-title");
+const yearInput = document.querySelector("#input-year");
+const result = document.querySelector("#result");
+
+searchBtn.addEventListener("click", loadApi);
+
+titleInput.addEventListener("keypress", resultByEnter);
+yearInput.addEventListener("keypress", resultByEnter);
+
+function loadApi() {
+  fetch(
+    "http://www.omdbapi.com/?apikey=167eb644&t=" +
+      titleInput.value +
+      "&y=" +
+      yearInput.value
+  )
+    .then((response) => response.json())
+    .then((data) => showResult(data));
+}
+
+function showResult(data) {
+  const resultTitle = (document.querySelector("#result-title").innerHTML =
+    "Title: " + data.Title);
+  const resultYear = (document.querySelector("#result-year").innerHTML =
+    "Year: " + data.Year);
+  const resultActors = (document.querySelector("#result-actors").innerHTML =
+    "Actors: " + data.Actors);
+  const resultGenre = (document.querySelector("#result-genre").innerHTML =
+    "Genre: " + data.Genre);
+  const resultProduction = (document.querySelector(
+    "#result-production"
+  ).innerHTML = "Production: " + data.Production);
+  const resultDirector = (document.querySelector("#result-director").innerHTML =
+    "Director: " + data.Director);
+  const resultCountry = (document.querySelector("#result-country").innerHTML =
+    "Country: " + data.Country);
+  const resultAwards = (document.querySelector("#result-awards").innerHTML =
+    "Awards: " + data.Awards);
+  const resultPlot = (document.querySelector("#result-plot").innerHTML =
+    "Plot: " + data.Plot);
+}
+
+function resultByEnter(e) {
+  if (e.key === "Enter") {
+    loadApi();
+  }
+}
